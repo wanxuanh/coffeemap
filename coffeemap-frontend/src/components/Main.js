e9bcfd9baa9b3c13b8bc1553c771650a2e255b91
wanxuanh <wanxuan.ho@gmail.com>
2022-05-21 12:10:13 +0800

    added nav bar

--- coffeemap-frontend/src/components/Main.js
+++ coffeemap-frontend/src/components/Main.js
@@ -1,11 +1,5 @@
-import NavBar from "./NavBar"
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import Nav from 'react-bootstrap/Nav'
+import NavBar from './NavBar';
 
 export default function Main() {
-	return (
-		<NavBar/>
-	);
+	return <NavBar />;
 }
