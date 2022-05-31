72e92d68c69ef27b630d5f9bdda173d91eea4303
wanxuanh <wanxuan.ho@gmail.com>
2022-05-31 15:33:34 +0800

    added 5 cafes in map

--- coffeemap-frontend/src/components/Map.js
+++ coffeemap-frontend/src/components/Map.js
@@ -2,10 +2,6 @@ import React from "react";
 import GoogleMapReact from 'google-map-react';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import PropTypes from 'prop-types';
-import shouldPureComponentUpdate from 'react-pure-render/function';
-import MyGreatPlace from './my_great_place.jsx';
-
-
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -18,9 +14,26 @@ export default function SimpleMap(){
     zoom: 20
     
   };
-    
-  shouldComponentUpdate = shouldPureComponentUpdate;
 
+  function createMapOptions(maps) {
+  // next props are exposed at maps
+  // "Animation", "ControlPosition", "MapTypeControlStyle", "MapTypeId",
+  // "NavigationControlStyle", "ScaleControlStyle", "StrokePosition", "SymbolPath", "ZoomControlStyle",
+  // "DirectionsStatus", "DirectionsTravelMode", "DirectionsUnitSystem", "DistanceMatrixStatus",
+  // "DistanceMatrixElementStatus", "ElevationStatus", "GeocoderLocationType", "GeocoderStatus", "KmlLayerStatus",
+  // "MaxZoomStatus", "StreetViewStatus", "TransitMode", "TransitRoutePreference", "TravelMode", "UnitSystem"
+  return {
+    zoomControlOptions: {
+      position: maps.ControlPosition.RIGHT_CENTER,
+      style: maps.ZoomControlStyle.SMALL
+    },
+    mapTypeControlOptions: {
+      position: maps.ControlPosition.TOP_RIGHT
+    },
+    mapTypeControl: true
+  };
+}
+    
 
   return (
     // Important! Always set the container height explicitly
@@ -30,14 +43,31 @@ export default function SimpleMap(){
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
         options={createMapOptions}>
-        <MyGreatPlace lat={1.27709} lng={103.851959} text={'A'} />
-    
-        <AnyReactComponent
+        
+         <AnyReactComponent
           lat={1.27709}
           lng={103.851959}
-          text="☕ Nylon Coffee"
-          
-
+          text="☕ Nylon"
+        />
+        <AnyReactComponent
+          lat={1.3128331674021738}
+          lng={103.86110632643916}
+          text="☕ Apartment Coffee"
+        />
+          <AnyReactComponent
+          lat={1.3656806822389873}
+          lng={103.86987143511345}
+          text="☕ Chu and Co"
+        />
+         <AnyReactComponent
+          lat={1.3119747944499036}
+          lng={103.79685794417453}
+          text="☕ Sunday Folks"
+        />
+           <AnyReactComponent
+          lat={1.2862391217210336}
+          lng={103.8034560399344}
+          text="☕ Rookie's Coffee Shop "
         />
       </GoogleMapReact>
     </div>
