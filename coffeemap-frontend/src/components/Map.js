53fdced5d7120450bb699c93488d6c066298120c
wanxuanh <wanxuan.ho@gmail.com>
2022-05-31 14:31:23 +0800

    style button marker

--- coffeemap-frontend/src/components/Map.js
+++ coffeemap-frontend/src/components/Map.js
@@ -2,19 +2,25 @@ import React from "react";
 import GoogleMapReact from 'google-map-react';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import PropTypes from 'prop-types';
-import { RedoSharp } from "@material-ui/icons";
+import shouldPureComponentUpdate from 'react-pure-render/function';
+import MyGreatPlace from './my_great_place.jsx';
 
 
-const AnyReactComponent = ({ text }) => <div style={{background:black, font:red}}>{text}</div>;
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function SimpleMap(){
   const defaultProps = {
     center: {
-      lat: 1.3128331674021738,
-      lng: 103.86110632643916
+      lat: 1.27709,
+      lng: 103.851959
     },
-    zoom: 15
+    zoom: 20
+    
   };
+    
+  shouldComponentUpdate = shouldPureComponentUpdate;
+
 
   return (
     // Important! Always set the container height explicitly
@@ -23,7 +29,9 @@ export default function SimpleMap(){
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
-      >
+        options={createMapOptions}>
+        <MyGreatPlace lat={1.27709} lng={103.851959} text={'A'} />
+    
         <AnyReactComponent
           lat={1.27709}
           lng={103.851959}
