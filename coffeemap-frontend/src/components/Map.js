d783accb74262fa3ec3bf8b571b3fae74a07faa5
wanxuanh <wanxuan.ho@gmail.com>
2022-05-31 14:11:26 +0800

    test markers

--- coffeemap-frontend/src/components/Map.js
+++ coffeemap-frontend/src/components/Map.js
@@ -12,7 +12,7 @@ export default function SimpleMap(){
       lat: 1.27709,
       lng: 103.851959
     },
-    zoom: 10
+    zoom: 13
   };
 
   return (
@@ -22,14 +22,36 @@ export default function SimpleMap(){
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
       >
         <AnyReactComponent
           lat={1.27709}
           lng={103.851959}
           text="☕ Nylon Coffee"
-          
-
         />
+        <AnyReactComponent
+          lat={1.3128331674021738}
+          lng={103.86110632643916}
+          text="☕ Apartment Coffee"
+        />
+          <AnyReactComponent
+          lat={1.3656806822389873}
+          lng={103.86987143511345}
+          text="☕ Chu and Co"
+        />
+         <AnyReactComponent
+          lat={1.3119747944499036}
+          lng={103.79685794417453}
+          text="☕ Sunday Folks"
+        />
+           <AnyReactComponent
+          lat={1.2862391217210336}
+          lng={103.8034560399344}
+          text="☕ Rookie's Coffee Shop "
+        />
+       
+
       </GoogleMapReact>
     </div>
   );
