b8685f9634db0f3bbae4156e2f6a71b1d2349539
wanxuanh <wanxuan.ho@gmail.com>
2022-05-31 12:54:19 +0800

    react google map with one marker

--- coffeemap-frontend/src/components/Map.js
+++ coffeemap-frontend/src/components/Map.js
@@ -1,6 +1,8 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import PropTypes from 'prop-types';
+
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -10,21 +12,21 @@ export default function SimpleMap(){
       lat: 1.27709,
       lng: 103.851959
     },
-    zoom: 100
+    zoom: 50
   };
 
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
         <AnyReactComponent
           lat={1.27709}
           lng={103.851959}
-          text="My Marker"
+          text="Nylon Coffee"
         />
       </GoogleMapReact>
     </div>
